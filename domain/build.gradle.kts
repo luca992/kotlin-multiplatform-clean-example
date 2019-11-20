0fa0168c32fdda492a583fd3a9ea9ada1d8fa0a0
Luca Spinazzola <mspinluca@gmail.com>
2019-11-19 19:49:49 -0800

    add coroutines to domain module

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -17,12 +17,33 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation(Libs.kotlin_stdlib_common)
+                implementation(Libs.kotlinx_coroutines_core_common)
             }
         }
 
         val jvmMain by getting {
             dependencies {
                 implementation(Libs.kotlin_stdlib_jdk8)
+                implementation(Libs.kotlinx_coroutines_core)
+            }
+        }
+
+        val nativeMain by creating {
+            dependsOn(commonMain)
+            dependencies {
+                implementation(Libs.kotlinx_coroutines_core_native)
+            }
+        }
+
+        val iosArm32Main by getting {}
+        val iosX64Main by getting {}
+        val iosArm64Main by getting {}
+        val macosX64Main by getting {}
+
+
+        configure(listOf(iosArm32Main,iosX64Main,iosArm64Main, macosX64Main)) {
+            dependsOn(nativeMain)
+            dependencies {
             }
         }
 
