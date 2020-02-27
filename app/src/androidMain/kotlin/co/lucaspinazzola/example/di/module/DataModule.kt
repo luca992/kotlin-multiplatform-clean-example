c975c28bf97c24dcd4b41e2b4cbc7f470e9ad309
Luca Spinazzola <mspinluca@gmail.com>
2020-02-27 15:41:21 -0800

    provide GiphyApi

--- app/src/androidMain/kotlin/co/lucaspinazzola/example/di/module/DataModule.kt
+++ app/src/androidMain/kotlin/co/lucaspinazzola/example/di/module/DataModule.kt
@@ -1,10 +1,19 @@
 package co.lucaspinazzola.example.di.module
 
 
+import co.lucaspinazzola.example.BuildConfig
+import co.lucaspinazzola.example.data.api.GiphyApi
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class DataModule {
+
+
+    @Provides
+    @Singleton
+    fun provideGiphyApi() : GiphyApi = GiphyApi(BuildConfig.GIPHY_API_KEY, BuildConfig.DEBUG)
 
 
     interface Exposes {
