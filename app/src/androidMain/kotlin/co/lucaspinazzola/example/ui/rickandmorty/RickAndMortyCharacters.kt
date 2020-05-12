e767a46f5567d731b54e3b8c9098f2a7da630dc0
Luca Spinazzola <mspinluca@gmail.com>
2020-05-12 16:33:36 -0700

    load images into two column table

--- app/src/androidMain/kotlin/co/lucaspinazzola/example/ui/rickandmorty/RickAndMortyCharacters.kt
+++ app/src/androidMain/kotlin/co/lucaspinazzola/example/ui/rickandmorty/RickAndMortyCharacters.kt
@@ -6,6 +6,7 @@ import androidx.ui.core.ContextAmbient
 import androidx.ui.core.Modifier
 import androidx.ui.foundation.VerticalScroller
 import androidx.ui.layout.Column
+import androidx.ui.layout.Table
 import androidx.ui.layout.height
 import androidx.ui.layout.width
 import androidx.ui.livedata.observeAsState
@@ -27,11 +28,24 @@ interface RickAndMortyCharacters {
             val vm = FragmentComponent.Initializer.init((context.applicationContext as ExampleApplication).mainComponent).factory.create(RickAndMortyCharactersViewModel::class.java)
             val imgs = vm.imgs.ld().observeAsState()
             VerticalScroller {
-                Column {
-                    imgs.value?.forEach { img ->
-                        CoilImage(img.url){
-                            placeholder(ResourcesCompat.getDrawable(context.resources, R.drawable.ic_search_black_24dp, null))
-                        }//LoadingDrawable(context))}
+                if (imgs.value!!.isNotEmpty()) {
+                    Table(columns = 2) {
+                        for (i in 0 until imgs.value!!.size / 2) {
+                            tableRow {
+                                for (j in 0 until 2) {
+                                    val img = imgs.value!![i * 2 + j]
+                                    CoilImage(img.url) {
+                                        placeholder(
+                                            ResourcesCompat.getDrawable(
+                                                context.resources,
+                                                R.drawable.ic_search_black_24dp,
+                                                null
+                                            )
+                                        )
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
