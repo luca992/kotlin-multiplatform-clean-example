79de3027dab514d5d1f983fbc134bce2d9ec7075
Luca Spinazzola <mspinluca@gmail.com>
2020-05-21 19:04:39 -0700

    create row pairs by observing flow

--- app/src/androidMain/kotlin/co/lucaspinazzola/example/ui/rickandmorty/RickAndMortyCharacters.kt
+++ app/src/androidMain/kotlin/co/lucaspinazzola/example/ui/rickandmorty/RickAndMortyCharacters.kt
@@ -1,46 +1,96 @@
 package co.lucaspinazzola.example.ui.rickandmorty
 
+import android.graphics.drawable.Drawable
 import androidx.compose.*
+import androidx.compose.frames.ModelList
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.ui.core.ContextAmbient
+import androidx.ui.core.DensityAmbient
 import androidx.ui.core.Modifier
+import androidx.ui.core.WithConstraints
 import androidx.ui.foundation.AdapterList
+import androidx.ui.foundation.Image
 import androidx.ui.foundation.ScrollerPosition
 import androidx.ui.foundation.VerticalScroller
-import androidx.ui.layout.fillMaxWidth
-import androidx.ui.layout.height
+import androidx.ui.graphics.ImageAsset
+import androidx.ui.graphics.asImageAsset
+import androidx.ui.layout.*
 //import androidx.ui.layout.Table
 import androidx.ui.livedata.observeAsState
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.IntPx
 import androidx.ui.unit.dp
+import androidx.ui.unit.toPx
 import co.lucaspinazzola.example.ExampleApplication
 import co.lucaspinazzola.example.R
 import co.lucaspinazzola.example.di.component.ViewComponent
+import co.lucaspinazzola.example.domain.model.Img
 import co.lucaspinazzola.example.ui.base.ComposableView
 import co.lucaspinazzola.example.ui.factory.ViewModelFactory
+import coil.Coil
+import coil.request.LoadRequest
+import coil.request.LoadRequestBuilder
+import coil.size.Scale
+import coil.target.Target
 import com.luca992.compose.image.CoilImage
 import com.squareup.inject.assisted.AssistedInject
+import dev.icerock.moko.mvvm.livedata.asFlow
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
 class RickAndMortyCharacters(val vm: RickAndMortyCharactersViewModel): ComposableView {
         //val ScrollerPosition.isAtEndOfList: Boolean get() = value >= maxPosition
 
+        @InternalCoroutinesApi
         @Composable
         override fun Content() {
             val context = ContextAmbient.current
-            val imgs = vm.imgs.ld().observeAsState()
-            if (imgs.value!!.isNotEmpty()) {
+            val pairedImgs = remember{ ModelList<Pair<Img,Img>>() }
+
+            val imgs = remember {
+                GlobalScope.launch(Dispatchers.Main) {
+                    vm.imgs.asFlow().collect { imgs:List<Img> ->
+                        pairedImgs.clear()
+                        for (i in imgs.indices step 2) {
+                            pairedImgs.add(Pair(imgs[i], imgs[i + 1]))
+                        }
+                    }
+                }
+            }
+
+            if (pairedImgs.isNotEmpty()) {
                 AdapterList(
-                    data = imgs.value!!,
-                    itemCallback = { img ->
-                        CoilImage(img.url) {
-                            placeholder(
-                                ResourcesCompat.getDrawable(
-                                    context.resources,
-                                    R.drawable.ic_search_black_24dp,
-                                    null
-                                )
-                            )
+                    data = pairedImgs,
+                    itemCallback = { imgPair ->
+                        WithConstraints {
+                            Row {
+                                val w = with(DensityAmbient.current) { (constraints.maxWidth.toDp().value / 2).dp }
+                                CoilImage(imgPair.first.url, Modifier.width(w)) {
+                                    placeholder(
+                                        ResourcesCompat.getDrawable(
+                                            context.resources,
+                                            R.drawable.ic_search_black_24dp,
+                                            null
+                                        )
+                                    )
+                                }
+                                CoilImage(imgPair.second.url, Modifier.width(w)) {
+                                    placeholder(
+                                        ResourcesCompat.getDrawable(
+                                            context.resources,
+                                            R.drawable.ic_search_black_24dp,
+                                            null
+                                        )
+                                    )
+                                }
+                            }
+
+
                         }
                     }
                 )
@@ -111,3 +161,4 @@ fun ProfilePreview() {
     val rmc = ViewComponent.Initializer.init((context.applicationContext as ExampleApplication).mainComponent).composableViewByClass()[RickAndMortyCharacters::class.java]
     rmc?.Content()
 }
+
