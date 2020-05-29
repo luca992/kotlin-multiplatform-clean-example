a3db4aafa69ad5443560014d8a8bbedc9f5eb2b3
Luca Spinazzola <mspinluca@gmail.com>
2020-05-29 15:15:57 -0700

    Add a gif search bar

--- app/src/androidMain/kotlin/co/lucaspinazzola/example/ui/giphy/GiphyComp.kt
+++ app/src/androidMain/kotlin/co/lucaspinazzola/example/ui/giphy/GiphyComp.kt
@@ -1,38 +1,40 @@
 package co.lucaspinazzola.example.ui.giphy
 
-import android.graphics.drawable.Animatable
-import android.graphics.drawable.Drawable
-import androidx.compose.*
+import androidx.compose.Composable
 import androidx.compose.frames.ModelList
+import androidx.compose.onActive
+import androidx.compose.remember
 import androidx.core.content.res.ResourcesCompat
-import androidx.core.graphics.drawable.toBitmap
 import androidx.ui.core.ContextAmbient
 import androidx.ui.core.DensityAmbient
 import androidx.ui.core.Modifier
 import androidx.ui.core.WithConstraints
 import androidx.ui.foundation.AdapterList
-import androidx.ui.foundation.Image
-import androidx.ui.graphics.ImageAsset
-import androidx.ui.graphics.asImageAsset
+import androidx.ui.foundation.TextField
+import androidx.ui.foundation.TextFieldValue
+import androidx.ui.graphics.Color
+import androidx.ui.input.KeyboardType
+import androidx.ui.layout.Column
 import androidx.ui.layout.Row
+import androidx.ui.layout.padding
 import androidx.ui.layout.width
+import androidx.ui.livedata.observeAsState
+import androidx.ui.text.TextRange
+import androidx.ui.text.TextStyle
 import androidx.ui.tooling.preview.Preview
-import androidx.ui.unit.IntPx
 import androidx.ui.unit.dp
+import androidx.ui.unit.sp
 import co.lucaspinazzola.example.ExampleApplication
 import co.lucaspinazzola.example.R
 import co.lucaspinazzola.example.di.component.ViewComponent
 import co.lucaspinazzola.example.domain.model.Img
 import co.lucaspinazzola.example.ui.base.ComposableView
-import coil.Coil
-import coil.request.LoadRequest
-import coil.request.LoadRequestBuilder
-import coil.size.Scale
-import dev.icerock.moko.mvvm.livedata.asFlow
-import kotlinx.coroutines.flow.collect
-import coil.target.Target
 import com.luca992.compose.image.CoilImage
-import kotlinx.coroutines.*
+import dev.icerock.moko.mvvm.livedata.asFlow
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
 class GiphyComp(val vm: GiphyViewModel): ComposableView {
@@ -41,9 +43,6 @@ class GiphyComp(val vm: GiphyViewModel): ComposableView {
         override fun Content() {
             val context = ContextAmbient.current
             val pairedImgs = remember { ModelList<Pair<Img, Img>>() }
-            remember {
-                vm.query.value = "test"
-            }
             remember {
                 GlobalScope.launch(Dispatchers.Main) {
                     vm.gifs.asFlow().collect { imgs: List<Img> ->
@@ -54,41 +53,59 @@ class GiphyComp(val vm: GiphyViewModel): ComposableView {
                     }
                 }
             }
+            val query = vm.query.ld().observeAsState()
 
-            AdapterList(
-                data = pairedImgs,
-                itemCallback = { imgPair ->
-                    WithConstraints {
-                        Row {
-                            val w = with(DensityAmbient.current) { (constraints.maxWidth.toDp().value / 2).dp }
-                            CoilImage(imgPair.first.url, Modifier.width(w)) {
-                                placeholder(
-                                    ResourcesCompat.getDrawable(
-                                        context.resources,
-                                        R.drawable.ic_search_black_24dp,
-                                        null
+            Column {
+
+                TextField(
+                    modifier = Modifier.padding(16.dp),
+                    value = TextFieldValue(
+                        query.value ?: "",
+                        TextRange(query.value?.length ?: 0, query.value?.length ?: 0)
+                    ),
+                    onValueChange = { vm.query.value = it.text },
+                    textStyle = TextStyle(
+                        fontSize = (20.sp),
+                        color = Color.DarkGray
+                    ),
+                    keyboardType = KeyboardType.Text
+                )
+                AdapterList(
+                    data = pairedImgs,
+                    itemCallback = { imgPair ->
+                        WithConstraints {
+                            Row {
+                                val w =
+                                    with(DensityAmbient.current) { (constraints.maxWidth.toDp().value / 2).dp }
+                                CoilImage(imgPair.first.url, Modifier.width(w)) {
+                                    placeholder(
+                                        ResourcesCompat.getDrawable(
+                                            context.resources,
+                                            R.drawable.ic_search_black_24dp,
+                                            null
+                                        )
                                     )
-                                )
+                                }
+                                CoilImage(imgPair.second.url, Modifier.width(w)) {
+                                    placeholder(
+                                        ResourcesCompat.getDrawable(
+                                            context.resources,
+                                            R.drawable.ic_search_black_24dp,
+                                            null
+                                        )
+                                    )
+                                }
                             }
-                            CoilImage(imgPair.second.url, Modifier.width(w)) {
-                                placeholder(
-                                    ResourcesCompat.getDrawable(
-                                        context.resources,
-                                        R.drawable.ic_search_black_24dp,
-                                        null
-                                    )
-                                )
+                        }
+                        onActive {
+                            if (pairedImgs.last() === imgPair) {
+                                //at end of list
+                                vm.loadNextPage()
                             }
                         }
                     }
-                    onActive {
-                        if (pairedImgs.last() === imgPair) {
-                            //at end of list
-                            vm.loadNextPage()
-                        }
-                    }
-                }
-            )
+                )
+            }
         }
 }
 
